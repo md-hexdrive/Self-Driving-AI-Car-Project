ca7f43c889ee0fc68cf472b0df9e4052c64008ca
md-hexdrive <pdotzert7@gmail.com>
2020-03-02 16:48:07 -0500

    Modifications to driving recorder code

--- Self-Driving Car Project/record_driving.py
+++ Self-Driving Car Project/record_driving.py
@@ -2,58 +2,81 @@ import driving
 import logging
 import cv2
 import os
+import os.path
 import time
-"""
-This program allows recording of the sequence of commands a user uses to control the car remotely.
-In addition, the sequence of commands and the length of time they were used for can be retrieved afterwards.
-The car can repeat the sequence of actions on its own.
+from threading import Thread
 
-Recorded data may also be used to help train the neural network later on.
+
+"""
+This program allows recording of driving data (the steering position of the car at every video frame)
+
+Recorded data will be used to train the neural network later on.
 """
 
+actions = ["lft", "rht", "mid", "fwd", "bck", "stp",]
+
+running = False
 class RecordDriving():
-    def __init__(this, source = 0, save_dir = '/home/pi/Desktop/recordings/%i/' % (int(time.time()))):
-        this.source = source
-        this.cap = cv2.VideoCapture(source)
-        this.recording_directory = save_dir
-        os.makedirs(recording_directory)
-        this.frameN = 0
+    def __init__(self, source = 0, save_dir = '/home/pi/Desktop/recordings/%i/' % (int(time.time()))):
+        self.source = source
+        self.cap = cv2.VideoCapture(self.source)
+        self.recording_directory = save_dir
+        
+        if not os.path.exists(self.recording_directory):
+            os.makedirs(self.recording_directory)
+        self.frameN = 0
 
-        this.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        this.out = cv2.VideoWriter('%svideo_01.avi' % (recording_directory), fourcc, 20.0, (640,480))
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('%svideo_01.avi' % (self.recording_directory),
+                                   self.fourcc, 20.0, (640,480))
 
-
-    def start_recording(this):
-        this.cap = cv2.VideoCapture(source)
-        this.recording_directory = save_dir
-        os.makedirs(recording_directory)
-        this.frameN = 0
-
-        this.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        this.out = cv2.VideoWriter('%svideo_01.avi' % (recording_directory), fourcc, 20.0, (640,480))
-
+    def start_recording(self):
+        running = True
+        self.record()
     
-    def record(this):
-        while (cap.isOpened()):
-            capture_frame()
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        stop_recording()
-    def stop_recording(this):
-        out.release()
-        cap.release()
+    def record(self):
+        while running:
+            
+            self.capture_frame()
+    
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+            
+        self.stop_recording()
+    def stop_recording(self):
+        self.out.release()
+        self.cap.release()
         cv2.destroyAllWindows()
     
-    def capture_frame(this):
-            this.frameN += 1
-            ret, frame = cap.read()
-            cv2.imshow('frame', frame)
-            out.write(frame)
+    def capture_frame(self):
+        self.frameN += 1
+        angle = self.curSteeringPos()
+        ret, frame = self.cap.read()
+        
+        overlay = frame
+        
+        height, width, _ = frame.shape
+        
+        if (angle == 0):
+            overlay = cv2.rectangle(frame, (0, height - 50), (width // 2, height), (0, 0, 255), -1)
+        
+        if (angle == 2):
+            overlay = cv2.rectangle(frame, (width // 2, height - 50), (width, height), (0, 255, 0), -1)
+        
+        if (angle == 1):
+            overlay = cv2.rectangle(frame, (0, height - 50), (width, height), (0, 128, 255), -1)
             
-            cv2.imwrite('%simage_%03i_%s.jpg' % (this.recording_directory, this.frameN, curSteeringPos()), frame)
+        cv2.imshow('overlay', overlay)
+        self.out.write(frame)
+        
+        cv2.imwrite('%simage_%03i_%i.jpg' % (self.recording_directory, self.frameN,
+                                             angle), frame)
 
     
-    def curSteeringPos(this):
+    def curSteeringPos(self):
+        pos = driving.getSteeringPos()
+        return pos
+    """
         if driving.isTurnedStraight():
             return "mid"
         elif driving.isTurnedLeft():
@@ -63,14 +86,21 @@ class RecordDriving():
         else:
             logging.debug("Incorrect Steering Position")
             return "err"
+    """
+def flight_recorder():
+    recorder = RecordDriving(0)
+    recorder.start_recording()
+    recorder.stop_recording()
+    del recorder
 
-recorder = RecordDriving(0)
-
+def run_recording_thread():
+    t1 = Thread(target=flight_recorder)
+    t1.run()
 commands = []
 command_number = 0
 
-actions = ["fwd", "bck", "stp", "lft", "rht", "mid"]
-
+if __name__ == '__main__':
+    run_recording_thread()
 
 
 
@@ -92,10 +122,3 @@ def returnCommandList():
 def playbackActions():
     pass
 
-logging.basicConfig(level=logging.DEBUG)
-
-
-addCommand("f", 100)
-addCommand("S", 20)
-addCommand('t', 1000)
-print(returnCommandList())
