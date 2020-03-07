0b5a0a5159f3567b1cfa2cb10db4fde244913b4e
md-hexdrive <pdotzert7@gmail.com>
2020-03-06 19:50:28 -0500

    Update drive_with_ai.py
    Fixed errors.
    Commented out ultrasonic sensor code for now.

--- Self-Driving Car Project/drive_with_ai.py
+++ Self-Driving Car Project/drive_with_ai.py
@@ -7,7 +7,7 @@ Some of this code was adapted from the DeepPiCar tutorial, specifically, end_to_
 """
 import driving
 import train_model
-import distance_monitoring
+#import distance_monitoring
 
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -18,11 +18,13 @@ import os
 import os.path
 import logging
 import sys
+
 logging.basicConfig(level=logging.ERROR)
 
 model_path = '/home/pi/Desktop/models/nvidia_model.h5'
-
-# ;-) -> "Run Flash, Run" == "Drive AI, Drive"; I'm a bit of a fan of "The Flash"
+driving_commands = ['drive forward, turn left', 'drive forward, turn straight',
+                    'drive forward, turn right', 'stop driving']
+# ;-) -> "Run Flash, Run" ~= "Drive AI, Drive"; I'm a bit of a fan of "The Flash"
 def drive_ai_drive(model_path=model_path):
     halt = False
     
@@ -34,7 +36,7 @@ def drive_ai_drive(model_path=model_path):
     model = tf.keras.models.load_model(model_path)
     
     while True:
-        frame, ret = cap.read()
+        ret, frame = cap.read()
         cv2.imshow('frame', frame)
         
         key = cv2.waitKey(1) & 0xFF
@@ -45,19 +47,24 @@ def drive_ai_drive(model_path=model_path):
         if not halt:
             frame = train_model.img_preprocess(frame)
             x = np.asarray([frame])
-            driving_command = abs(model.predict(x)[0])
+            driving_command = int(model.predict(x)[0] + 0.5)
+            print(driving_command)
+            #print(driving_commands[driving_command])
             
             if driving_command == 3:
                 print("end of track")
                 driving.stopDriving()
             
             else:
+                driving.driveForward()
+                """
                 driving.setSteeringPos(driving_command)
                 
-                if distance_montoring.ultrasonic_sensor.distance < .45:
+                if distance_monitoring.ultrasonic_sensor.distance < .45:
                     print("stopped")
                 else:
                     driving.driveForward()
+                """
         
         else:
             driving.stopDriving()
