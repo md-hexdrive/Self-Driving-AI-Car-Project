5f9ea54c659b37594db1a70f685c29330c83fb7a
md-hexdrive <pdotzert7@gmail.com>
2020-03-11 11:52:55 -0400

    Update drive_with_ai.py
    Re-enabled distance monitoring.
    Added recording of self-driving attempts.
    
    The neural network model was modified to produce more certain outputs; therefore, the retrieving of the model results had to be modified.

--- Self-Driving Car Project/drive_with_ai.py
+++ Self-Driving Car Project/drive_with_ai.py
@@ -7,7 +7,7 @@ Some of this code was adapted from the DeepPiCar tutorial, specifically, end_to_
 """
 import driving
 import train_model
-#import distance_monitoring
+import distance_monitoring
 
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -18,10 +18,11 @@ import os
 import os.path
 import logging
 import sys
+import time
 
 logging.basicConfig(level=logging.ERROR)
 
-model_path = '/home/pi/Desktop/models/nvidia_model.h5'
+model_path = '/home/pi/Desktop/models/nvidia_model_final.h5'
 driving_commands = ['drive forward, turn left', 'drive forward, turn straight',
                     'drive forward, turn right', 'stop driving']
 # ;-) -> "Run Flash, Run" ~= "Drive AI, Drive"; I'm a bit of a fan of "The Flash"
@@ -29,46 +30,55 @@ def drive_ai_drive(model_path=model_path):
     halt = False
     
     cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('/home/pi/Desktop/recordings/test_drive_%i.avi' % int(time.time()),
+                          fourcc, 20.0, (640, 480))
     
     if not os.path.exists(model_path):
         logging.error(model_path, "does not exist, exiting")
         sys.exit()
     model = tf.keras.models.load_model(model_path)
+    print(model.summary())
     
     while True:
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
+        out.write(frame)
         
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
-        elif key == ord(' '):
+        elif key == ord(' '): # allow space key to stop the car
             halt = not halt
         if not halt:
             frame = train_model.img_preprocess(frame)
             x = np.asarray([frame])
-            driving_command = int(model.predict(x)[0] + 0.5)
-            print(driving_command)
-            #print(driving_commands[driving_command])
+            driving_command = np.argmax(model.predict(x)[0])
+            
             
             if driving_command == 3:
                 print("end of track")
                 driving.stopDriving()
             
             else:
-                driving.driveForward()
-                """
-                driving.setSteeringPos(driving_command)
-                
+                #driving.driveForward()
+
                 if distance_monitoring.ultrasonic_sensor.distance < .45:
-                    print("stopped")
+                    driving.stopDriving()
+                    print("object too close, stopping")
                 else:
+                    driving.setSteeringPos(driving_command)
                     driving.driveForward()
-                """
+                    #print(driving_command)
+                    print(driving_commands[driving_command])
+                
         
         else:
             driving.stopDriving()
+            print("stopped")
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
-
+    sys.exit()
+   
 drive_ai_drive()
