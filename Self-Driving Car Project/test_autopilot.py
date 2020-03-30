4b5a0f732b495dd2d426c5865820dfec76982559
md-hexdrive <pdotzert7@gmail.com>
2020-03-30 16:05:02 -0400

    Change test recording

--- Self-Driving Car Project/test_autopilot.py
+++ Self-Driving Car Project/test_autopilot.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     recording_dir = '/home/pi/Desktop/recordings/testing'
     #specific_dir = '200226_162200'
     #video_date = '200226_162200'
-    video_date = '2002_27_085915'
+    video_date = '2002_27_222155'
     video_name = 'video_' + video_date + '.avi'
     video_path = os.path.join(recording_dir, video_date, video_name)
     test_video(video_path)
